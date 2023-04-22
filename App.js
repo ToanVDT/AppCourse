e04d390a06ea23e7c0d35988b57ce6641fd24665
Toan007 <vanthanhtoan771@gmail.com>
2023-04-23 01:17:54 +0700

    commit 2

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
 // @ts-ignore
-import AppNavigation from "./src/Navigations";
+import AppNavigation from "./src/Navigations/index";
 export default function App() {
   return (
       <AppNavigation />
